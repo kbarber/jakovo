de9ecc5baaac3d417de6b4aa794319b6baab7ef4
Ken Barber <ken@bob.sh>
2012-01-15 00:51:57 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jakovo::Application.config.secret_token = 'e477eb0e973af05cac61fda7930f2446a7a13aa618b70a337b0da2efb859704538faa15d28e744f6f37958ea6890c2ed6e85ab854b105998301ce504f847e429'
